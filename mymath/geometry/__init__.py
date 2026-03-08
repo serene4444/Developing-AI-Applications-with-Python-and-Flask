a8c1ca8bd52d5f2486a6f2e90594412deef36cbc
Plummer, Serene <sereneplummer@my.unt.edu>
2026-03-08 14:45:02 -0500

    Add geometry module with area calculation functions

---
+++ mymath/geometry/__init__.py
@@ -0,0 +1,13 @@
+import math
+
+def area_of_rectangle(length, breadth):
+    """
+    This function returns the area of a rectangle given length and breadth
+    """
+    return length * breadth
+
+def area_of_circle(radius):
+    """
+    This function returns the area of a circle given radius
+    """
+    return math.pi * radius ** 2
